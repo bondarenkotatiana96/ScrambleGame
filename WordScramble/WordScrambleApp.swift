f08fa24dd8e6f1ef04c0d19292bc4a533f4d3bcc
Tatiana Bondarenko <62190724+bondarenkotatiana96@users.noreply.github.com>
2023-04-20 21:32:57 -0700

    Add files via upload

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Tatiana Bondarenko on 4/20/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
